a2b158b06ed5bfa69e1f8c59d6587d89870aa902
merve.dagli <merve.dagli@overtech.com.tr>
2021-05-09 04:05:29 +0300

    MVC Projesi

---
+++ UdemyNLayerProject.Web/DTOs/PersonDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Web.DTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
+}
---
+++ UdemyNLayerProject.Web/DTOs/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Web.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="{0} field is required.")]
+        public string Name { get; set; }
+        [Range(1,int.MaxValue,ErrorMessage = "{0} field must be a value greater than 1.")]  //burda required yazmamıza gerek yok çünkü in zaten varsayılanda sıfırdır.
+        public int Stock { get; set; }
+        [Range(1, double.MaxValue , ErrorMessage = "{0} field must be a value greater than 1.")]
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }  //Categorye bağlı olacağından dolayı
+    }
+}
